4166a9a830acbd3081343d33894388137f167bf9
mansi072003 <mansiagc36@gmail.com>
2025-03-19 23:54:39 +0530

    .
    update

---
+++ Day_3/Readme.md
@@ -0,0 +1,3 @@
+### 1. Investigate Circular Imports
+
+1
--- Day_3/module_a.py
+++ Day_3/module_a.py
@@ -1,5 +1,6 @@
 # 1. Investigate Circular Imports
 # Fix the circular import problem between module_a.py and module_b.py.
+# module_a.py file
 def func_a():
     return "Function A"
 
@@ -8,34 +9,3 @@ def call_func_b():
     return func_b()
 
 print(call_func_b())
-
-
-# 2. Dynamic Module Loading
-# Write a program that dynamically imports and executes a function from any module specified by the user.
-
-import importlib
-
-# Get user input
-module_name = input("Enter module name: ")  
-function_name = input("Enter function name: ") 
-argument = float(input("Enter argument: ")) 
-
-try:
- 
-    module = importlib.import_module(module_name)
-
-    func = getattr(module, function_name)
-    
-    # Execute function and print result
-    result = func(argument)
-    print("Output:", result)
-
-except ModuleNotFoundError:
-    print("Error: Module not found!")
-except AttributeError:
-    print("Error: Function not found in module!")
-except Exception as e:
-    print("Error:", e)
-
-# 3. Custom Module with Exception Handling
-
